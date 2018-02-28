383bc1763e10c93518b08ad97333674e26a867a0
Olexandr Stecuk <sandrostecuk@gmail.com>
2018-02-28 11:39:36 +0200

    first

---
+++ page/animation/my.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+	 var btnShow = document.querySelector('.show-alert');
+	 var btnHide = document.querySelector('.hide-alert');
+	 var divAlert = document.querySelector('.anim-target');
+
+	btnHide.addEventListener('click', function(){
+		alert(12);
+	});
+
+}
